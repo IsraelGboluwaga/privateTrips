966336e692afd60d5f84fe82d5d92e5a08754b76
gold_olar <sam99kupo@gmail.com>
2019-02-11 00:48:45 +0100

    want to start auth

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 if(process.env.NODE_ENV  === 'production'){
 	module.exports ={
-		mongoURI:'mongodb://admin:saamukupo99@ds127545.mlab.com:27545/todo-app'
-
+		mongoURI:'mongodb://admin:saamukupo99@ds127545.mlab.com:27545/privatetrips'
+ 
 	}
 }else{
 		module.exports = {
 		mongoURI :'mongodb://localhost/private-trips'
 	}
 }
+
